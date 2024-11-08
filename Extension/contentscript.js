240af305c0bbff809435c9655450a9b5af02c0f1
Now41 <aotnow41@gmail.com>
2024-11-08 20:59:51 +0900

    backup

--- Extension/contentscript.js
+++ Extension/contentscript.js
@@ -1,8 +1,9 @@
+// contentScript.js
+
 (async () => {
-  // 페이지의 전체 HTML 추출
+  // Extract page HTML and images
   const pageHtml = document.documentElement.outerHTML;
 
-  // 이미지 태그(<img>)를 찾아서 src 속성의 데이터를 base64 형식으로 변환하여 저장
   const images = await Promise.all(Array.from(document.images).map((img) => {
     const imageUrl = img.src;
 
@@ -10,13 +11,16 @@
       .then((response) => response.blob())
       .then((blob) => new Promise((resolve, reject) => {
         const reader = new FileReader();
-        reader.onloadend = () => resolve(reader.result);
+        reader.onloadend = () => {
+          const base64String = reader.result;
+          if (base64String && base64String.startsWith("data:image/")) {
+            resolve({ src: imageUrl, base64: base64String });
+          } else {
+            resolve(null);
+          }
+        };
         reader.onerror = reject;
-        reader.readAsDataURL(blob); // blob을 base64 형식으로 변환
-      }))
-      .then((base64Data) => ({
-        src: imageUrl,
-        base64: base64Data
+        reader.readAsDataURL(blob);
       }))
       .catch((error) => {
         console.error('Error converting image:', error);
@@ -24,18 +28,15 @@
       });
   }));
 
-  // base64 변환이 성공한 이미지들만 필터링
   const filteredImageData = images.filter(Boolean);
 
-  // 서버로 HTML, 이미지 데이터를 통합하여 전송
+  // Send HTML and image data to background.js
   const requestData = {
     html: pageHtml,
     images: filteredImageData,
-    screenshot: null // 여기서 화면 캡처 데이터는 background.js에서 추가
   };
 
-  // background.js에 메시지를 보내 데이터 전송을 요청
-  chrome.runtime.sendMessage({ action: "sendPageData", data: requestData }, (response) => {
+  chrome.runtime.sendMessage({ action: "sendData", data: requestData }, (response) => {
     if (response && response.success) {
       console.log("Data sent successfully");
     } else {
