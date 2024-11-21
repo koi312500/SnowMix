c3dafab9ee4b8547fb73c52a4a6433d542564a1a
KOI3125 <koi312500@gmail.com>
2024-11-21 11:41:51 +0900

    fix: fix port number of proxy server

--- Proxy_Server/index.js
+++ Proxy_Server/index.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const request = require('request');
 const app = express();
-const PORT = 3000; // 서버가 실행될 포트 번호
+const PORT = 4000; // 서버가 실행될 포트 번호
 
 // CORS를 허용하는 미들웨어
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    
+
     // OPTIONS 메서드 요청은 바로 응답
     if (req.method === 'OPTIONS') {
         return res.sendStatus(200);
     }
-    
+
     next();
 });
 
@@ -32,7 +32,7 @@ app.get('/proxy', (req, res) => {
             if (error) {
                 return res.status(500).send('Error fetching image');
             }
-            
+
             // CORS 헤더 추가
             res.set('Content-Type', response.headers['content-type']);
             res.send(body);
